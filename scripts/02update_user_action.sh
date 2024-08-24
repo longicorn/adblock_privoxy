679862046fc1f44f663ae9627ad69cdf25adb033
longicorn <longicorn.c@gmail.com>
2024-08-24 15:29:17 +0900

    feat!: fix hosts_adblock.txt by hblock.molinero.dev

---
+++ scripts/02update_user_action.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cat /hosts/my_adblock.hosts >> /etc/privoxy/user.action
+
+wget https://hblock.molinero.dev/hosts_adblock.txt -O /tmp/hosts_adblock.txt
+grep '||' /tmp/hosts_adblock.txt |sed -e 's/||//g' | sed -e 's/\^//g' >> /etc/privoxy/user.action
