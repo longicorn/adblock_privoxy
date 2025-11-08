43f91aa7a3a066882f1ea97c7c1862bea5506562
longicorn <longicorn.c@gmail.com>
2025-11-08 21:57:28 +0900

    feat!: unblock feature

--- scripts/02update_user_action.sh
+++ scripts/02update_user_action.sh
@@ -3,12 +3,19 @@
 echo '{ +block{scripts, ads} }'>> /etc/privoxy/user.action
 
 rm -f /tmp/blocklist.txt
-cat /hosts/my_adblock.hosts > /tmp/blocklist.txt
+grep -v '#' /hosts/my_adblock.hosts|grep -v ^$ > /tmp/blocklist.txt
 
 wget https://hblock.molinero.dev/hosts_adblock.txt -O /tmp/hosts_adblock.txt
-grep '||' /tmp/hosts_adblock.txt |sed -e 's/||//g' | sed -e 's/\^//g' >> /tmp/blocklist.txt
+grep '||' /tmp/hosts_adblock.txt |sed -e 's/||//g' | sed -e 's/\^//g' | grep -v bsky.app >> /tmp/blocklist.txt
 
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -O /tmp/StevenBlack_hosts.txt
 grep 0.0.0.0 /tmp/StevenBlack_hosts.txt |awk '{print $2}'|grep -v 0.0.0.0 >> /tmp/blocklist.txt
+
+grep -v '#' /hosts/my_unblock.hosts|grep -v ^$ > /tmp/my_unblock.hosts
+cp /tmp/blocklist.txt /tmp/blocklist2.txt
+while read line; do
+  grep -v $line /tmp/blocklist2.txt > /tmp/a; mv /tmp/a /tmp/blocklist2.txt
+done < /tmp/my_unblock.hosts
+mv /tmp/blocklist2.txt /tmp/blocklist.txt
 
 sort /tmp/blocklist.txt | uniq >> /etc/privoxy/user.action
